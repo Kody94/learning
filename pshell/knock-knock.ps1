d7c94a7e7614aadb3de8f5b45748272ed675699b
Kody Striplin <kody094@gmail.com>
2018-04-02 09:36:49 -0400

    knock knock joke from ch1

---
+++ pshell/knock-knock.ps1
@@ -0,0 +1,43 @@
+# First joke
+Clear-Host
+$userReply = ""
+while ($userReply -ne "Who's there?") {
+  $userReply = Read-Host "Knock knock!"
+}
+Clear-Host
+while ($userReply -ne "Orange who?"){
+  $userReply = Read-Host "Orange."
+}
+Clear-Host
+Write-Output "Orange you glad you created this PowerShell script?"
+Start-Sleep -Seconds 5
+
+# First joke
+Clear-Host
+while ($userReply -ne "Who's there?") {
+  $userReply = Read-Host "Knock knock!"
+}
+Clear-Host
+while ($userReply -ne "Orange who?"){
+  $userReply = Read-Host "Orange."
+}
+Clear-Host
+Write-Output "Oranges are oranges, but this is a PowerShell script!"
+Start-Sleep -Seconds 5
+
+# First joke
+Clear-Host
+while ($userReply -ne "Who's there?") {
+  $userReply = Read-Host "Knock knock!"
+}
+Clear-Host
+while ($userReply -ne "Banana who?"){
+  $userReply = Read-Host "Banana."
+}
+Clear-Host
+Write-Output "Orange you glad I didn't say banana?"
+Start-Sleep -Seconds 5
+
+# Credit
+Clear-Host
+Write-Output "Created by Kody Striplin 2018"
